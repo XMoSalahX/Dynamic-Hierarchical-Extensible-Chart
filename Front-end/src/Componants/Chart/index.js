59aea103e4502aada4fac60ab0b7da418d89d2f3
XMoSalahX <01154615199>
2022-09-07 05:49:37 +0200

    Improve ui & reduce Request in drage & drop to 50%

--- Front-end/src/Componants/Chart/index.js
+++ Front-end/src/Componants/Chart/index.js
@@ -35,6 +35,7 @@ export default function Chart({ chartData }) {
       // Store Node ID in our session storage
       nodeEl.addEventListener("dragstart", () => {
         window.sessionStorage.setItem("NodeID", nodeEl.id);
+        nodeEl.style.opacity = 0.2;
       });
 
       // Store Parent ID in our session storage
@@ -44,23 +45,51 @@ export default function Chart({ chartData }) {
 
       // Get & Send Node ID and Parend ID to our DB
       nodeEl.addEventListener("dragend", async (e) => {
+        nodeEl.style.opacity = 1;
         let _id = window.sessionStorage.getItem("NodeID");
         _id = _id.substring(3, _id.length);
         let Parent_id = window.sessionStorage.getItem("containerId");
         Parent_id = Parent_id.substring(3, Parent_id.length);
-        const data = {
-          _id,
-          Parent_id,
-        };
-        fetch(`${config.server}/movenodes`, {
-          method: "PUT",
-          headers: {
-            "Content-type": "application/json; charset=UTF-8",
-          },
-          body: JSON.stringify(data),
-        }).then(() => {
-          window.location.href = "/";
-        });
+
+        if (_id !== Parent_id) {
+          const childNode = document.querySelector(`#id-${_id}`).parentElement
+            .parentElement;
+
+          // If we Will remove last child, go to remove parent node
+          if (childNode.parentElement.childElementCount === 1) {
+            childNode.parentElement.remove();
+          }
+
+          const parentNode = document.querySelector(`#id-${Parent_id}`)
+            .parentElement.parentElement;
+
+          childNode.remove();
+
+          // Check if we have ul tag already in our parent or Not
+          if (parentNode.querySelector("ul") == null) {
+            const ulClass = parentNode.parentElement.classList[0];
+            const ulElement = document.createElement("ul");
+            ulElement.classList.add(ulClass);
+            ulElement.appendChild(childNode);
+            parentNode.appendChild(ulElement);
+          } else {
+            parentNode.lastChild.appendChild(childNode);
+          }
+
+          const data = {
+            _id,
+            Parent_id,
+          };
+          // Check if Current Node in New Container Or Not. if in New Container Make A request.
+
+          fetch(`${config.server}/movenodes`, {
+            method: "PUT",
+            headers: {
+              "Content-type": "application/json; charset=UTF-8",
+            },
+            body: JSON.stringify(data),
+          });
+        }
       });
 
       // Check If our Curren Object Have a Node Child or Not
